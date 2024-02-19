279a34de4a7e523483d569d580a961adf6c94010
fahminlb33 <fahminlb33@gmail.com>
2024-02-19 10:19:52 +0700

    feat: add models for feature selection

---
+++ transform/models/marts/ml/marts_ml_train_sel_manual.sql
@@ -0,0 +1,36 @@
+{{ config(materialized='external', format='parquet') }}
+
+WITH base AS (
+	SELECT
+		carport,
+		dapur,
+		daya_listrik,
+		facility_ac,
+		facility_keamanan,
+		facility_laundry,
+		facility_masjid,
+		house_mat_bata_hebel,
+		house_mat_bata_merah,
+		jumlah_lantai,
+		kamar_mandi,
+		kamar_mandi_pembantu,
+		kamar_pembantu,
+		kamar_tidur,
+		lebar_jalan,
+		luas_bangunan,
+		luas_tanah,
+		ruang_makan,
+		ruang_tamu,
+		tag_cash_bertahap,
+		tag_komplek,
+		tag_kpr,
+		tag_perumahan,
+		tahun_dibangun,
+	FROM
+		{{ ref('int_ml_feature_outlier_removal') }}
+)
+
+SELECT
+	*
+FROM
+	base
---
+++ transform/models/marts/ml/marts_ml_train_sel_pvalue.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='external', format='parquet') }}
+
+{%- set P_VALUE_REJECT = 0.05 -%}
+{%- set EXCLUDE_COLUMNS = ['id', 'installment', 'district', 'city', 'description', 'url', 'last_modified', 'scraped_at'] -%}
+
+{%- set query_significant_features -%}
+	SELECT variable FROM {{ ref('marts_dashboard_correlations') }} WHERE p_value < {{ P_VALUE_REJECT }}
+{%- endset -%}
+
+{%- set results = run_query(query_significant_features) -%}
+{%- if execute -%}
+	{%- set significant_features = results.columns[0].values() -%}
+{%- else -%}
+	{%- set significant_features = [] -%}
+{%- endif -%}
+
+WITH base AS (
+	SELECT
+		{% for feature in significant_features %}
+			{%- if "spatial" not in feature and feature not in EXCLUDE_COLUMNS -%}
+				{{ feature }}{{ ', ' if not loop.last }}
+			{%- endif -%}
+		{% endfor %}
+	FROM
+		{{ ref('int_ml_feature_outlier_removal') }}
+)
+SELECT
+	*
+FROM
+	base
