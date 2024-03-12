973861ca49918e6cf7719712d4a96ce513f62f14
fahminlb33 <fahminlb33@gmail.com>
2024-03-12 23:19:27 +0700

    feat: add image proxy

--- app/pages/Tanya_AI.py
+++ app/pages/Tanya_AI.py
@@ -33,11 +33,13 @@ def load_css():
     with open("assets/style.css") as f:
         return f.read()
 
+
 @st.cache_resource()
 def proxy_image(url: str):
     # set headers
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
     }
 
     # get image
@@ -45,6 +47,7 @@ def proxy_image(url: str):
 
     # return image
     return response.content
+
 
 def render_chat(message: ChatRecord):
     with st.chat_message(message.role):
